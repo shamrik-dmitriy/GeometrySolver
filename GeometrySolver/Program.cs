41f17f4bb3d96ad442321a86c1a3f87a14dc029c
shamrik-dmitriy <@FscHRgA+Hfvr2pa>
2025-03-30 23:20:26 +0600

    + Добавил основной кусочек для зауска

---
+++ GeometrySolver/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Geometry.Models;
+
+namespace GeometrySolver
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new SquareSolver(new[] { new Point(0, 1), new Point(1, 0), new Point(0, 0), new Point(1, 1) });
+            Console.WriteLine($"Площадь квадрата: {square.GetArea()}, Периметр квадрата {square.GetPerimeter()}");
+        }
+    }
+}
