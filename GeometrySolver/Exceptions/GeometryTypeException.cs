3067654541abdebb0ce111782de94ec78cc1f668
shamrik-dmitriy <@FscHRgA+Hfvr2pa>
2025-03-30 23:19:27 +0600

    + Добавил кастомное исключение

---
+++ GeometrySolver/Exceptions/GeometryTypeException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GeometrySolver.Exceptions
+{
+    [Serializable]
+    public class GeometryTypeException : Exception
+    {
+        public GeometryTypeException() : base()
+        {
+        }
+
+        public GeometryTypeException(string message) : base(message)
+        {
+        }
+
+        public GeometryTypeException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
