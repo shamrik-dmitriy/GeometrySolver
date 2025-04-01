909fb51d170bb364bb362644efbd264addc6d931
shamrik-dmitriy <@FscHRgA+Hfvr2pa>
2025-04-01 23:22:26 +0600

    + Добавил метод расширения для проверки идентичности значений типа double с заданной точностью по умолчанию (1e-6)

---
+++ GeometrySolver/Extensions/DoubleExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GeometrySolver.Extensions
+{
+    public static class DoubleExtensions
+    {
+        /// <summary>
+        /// Сравнивает два числа вещественного типа с заданной точностью
+        /// </summary>
+        /// <param name="a">Первый параметр</param>
+        /// <param name="b">Второй параметр</param>
+        /// <param name="precision">Точность. По умолчанию 1e-6</param>
+        /// <returns>True - значения равны, False - значения не равны</returns>
+        public static bool CompareToPrecision(this double a, double b, double precision = 1e-6)
+        {
+            return Math.Abs(a - b) < precision;
+        }
+    }
+}
