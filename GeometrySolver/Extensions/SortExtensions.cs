8751cdc22af703973fa310a4c8310b48f5417d3f
shamrik-dmitriy <@FscHRgA+Hfvr2pa>
2025-04-01 23:23:04 +0600

    + Добавил метод расширения для сортировки точек по Х и У

--- GeometrySolver/Extensions/SortExtensions.cs
+++ GeometrySolver/Extensions/SortExtensions.cs
@@ -22,5 +22,15 @@ namespace GeometrySolver.Extensions
             return points.OrderBy(p =>
                 Math.Atan2(p.Y - center.Y, p.X - center.X)).ToArray();
         }
+
+        /// <summary>
+        /// Сортирует точки по X, затем по Y. Если сортировка по Х имеет одинаковые точки, то такие значения будут отсортированы по Y
+        /// </summary>
+        /// <param name="points">Набор точек</param>
+        /// <returns>Отсортированный набор точек</returns>
+        public static IEnumerable<Point> SortByXY(this IEnumerable<Point> points)
+        {
+            return points.OrderBy(p => p.X).ThenBy(p => p.Y).ToList();
+        }
     }
 }
