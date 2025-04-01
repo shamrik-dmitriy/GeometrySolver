2a7404ab484b9240c6b1cc3aee40bf7ccbba38cd
shamrik-dmitriy <@FscHRgA+Hfvr2pa>
2025-04-01 23:26:19 +0600

    + Добавил тесты для проверки работоспособности функции вычисления периметра квадрата

--- GeometrySolver.Tests/SquareSolverTests.cs
+++ GeometrySolver.Tests/SquareSolverTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using FluentAssertions;
 using Geometry.Models;
 using GeometrySolver.Exceptions;
+using GeometrySolver.Extensions;
 using Xunit;
 
 namespace GeometrySolver.Tests
@@ -38,18 +39,22 @@ namespace GeometrySolver.Tests
             }
         }
 
-        public static TheoryData<IEnumerable<Point>, double> ValidSquarePointsData =>
+        public static TheoryData<IEnumerable<Point>, double> ValidSquarePointsDataForPerimeter =>
             new TheoryData<IEnumerable<Point>, double>
             {
-                { new List<Point>() { new Point(0, 1), new Point(1, 0), new Point(0, 0), new Point(1, 1) }, 4 },
+                { new List<Point>() { new(0, 1), new(1, 0), new(0, 0), new(1, 1) }, 4 },
+                { new List<Point>() { new(0, -1), new(-1, 0), new(0, 0), new(-1, -1) }, 4 },
+                { new List<Point>() { new(-2.4, -1.3), new(1.3, -1.3), new(1.3, 2.4), new(-2.4, 2.4) }, 14.8 },
+                { new List<Point>() { new(-3.7, -4.5), new(3.1, -4.5), new(3.1, 2.3), new(-3.7, 2.3) }, 27.2 },
             };
 
         [Theory]
-        [MemberData(nameof(ValidSquarePointsData))]
+        [MemberData(nameof(ValidSquarePointsDataForPerimeter))]
         public void TestPerimeters(IEnumerable<Point> points, double expected)
         {
             var squareSolver = new SquareSolver(points);
-            squareSolver.GetPerimeter().Should().Be(expected);
+            var perimeter = squareSolver.GetPerimeter();
+            Assert.True(perimeter.CompareToPrecision(expected));
         }
 
         public void TestAreas(IEnumerable<Point> points, double expected)
