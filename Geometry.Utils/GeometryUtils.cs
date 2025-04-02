27eb5af41406b4ac6e296cdf01744d0080619d27
shamrik-dmitriy <@FscHRgA+Hfvr2pa>
2025-04-02 23:31:32 +0600

    + Добавил в геометрические утилиты метод проверки ска

--- Geometry.Utils/GeometryUtils.cs
+++ Geometry.Utils/GeometryUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Common.Extensions;
 using Geometry.Models;
 
 namespace Geometry.Utils
@@ -25,6 +26,23 @@ namespace Geometry.Utils
         public static double GetDistance(Point p1, Point p2)
         {
             return Math.Sqrt(GetDistanceSquared(p1, p2));
+        }
+
+        /// <summary>
+        /// Проверяет перпендикулярны ли два вектора между собой
+        /// </summary>
+        /// <param name="p1">Начальная точка первого вектора</param>
+        /// <param name="p2">Конечная точка первого вектора</param>
+        /// <param name="p3">Начальная точка второго вектора</param>
+        /// <param name="p4">Конечная точка второго вектора</param>
+        /// <returns>True - векторы перпендикулярны, False - векторы не перпендикулярны</returns>
+        public static bool IsPerpendicularVectors(Point p1, Point p2, Point p3, Point p4)
+        {
+            var firstVector = new Point(p2.X - p1.X, p2.Y - p1.Y);
+            var secondVector = new Point(p4.X - p3.X, p4.Y - p3.Y);
+
+            var isPerpendicular = firstVector.X * secondVector.X + firstVector.Y * secondVector.Y;
+            return isPerpendicular.CompareToPrecision(0);
         }
     }
 }
