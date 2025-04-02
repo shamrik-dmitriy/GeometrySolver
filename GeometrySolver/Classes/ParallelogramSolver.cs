c8b04d145e64036dfbb5e37ce7bfbdbc58cef293
shamrik-dmitriy <@FscHRgA+Hfvr2pa>
2025-04-02 22:53:54 +0600

    + Выделил базовый класс параллелограмма

---
+++ GeometrySolver/Classes/ParallelogramSolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Geometry.Models;
+using GeometrySolver.Interfaces;
+
+namespace GeometrySolver
+{
+    public abstract class ParallelogramSolver : AFigure, IFigure
+    {
+        public IEnumerable<Point> Points { get; }
+
+        public ParallelogramSolver(IEnumerable<Point> points)
+        {
+            Points = points;
+        }
+
+        public override void Validate()
+        {
+            if (!Points.Any())
+                throw new ArgumentException("Количество точек должно быть больше 0");
+
+            if (Points.Count() != 4)
+                throw new InvalidOperationException("Параллелограмм должен иметь 4 точки");
+        }
+
+        public abstract double GetArea();
+        public abstract double GetPerimeter();
+
+        public override string ToString()
+        {
+            return "Объект - параллелограмм";
+        }
+    }
+}
